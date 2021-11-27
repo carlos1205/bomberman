9068cf8fe80e6428860038f21ef2be5582fcd3ba
Carlos1205 <carloslima68.cs@gmail.com>
2021-11-27 03:32:57 -0300

    add bombe

--- Player.gd
+++ Player.gd
@@ -1,4 +1,6 @@
 extends Area2D
+signal hit
+signal plant_bombe
 
 export var speed = 400
 var screen_size
@@ -6,6 +8,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 func _process(delta):
 	var velocity = Vector2()
@@ -41,3 +44,20 @@ func _process(delta):
 			$AnimatedSprite.animation = "still-front"
 		elif "walk-side" == $AnimatedSprite.animation:
 			$AnimatedSprite.animation = "still-side"
+
+func _input(event):
+	if event.is_action_pressed("plant"):
+		plantBombe()
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = true
+
+func _on_Area2D_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disable", true)
+
+func plantBombe():
+	emit_signal("plant_bombe", position)
